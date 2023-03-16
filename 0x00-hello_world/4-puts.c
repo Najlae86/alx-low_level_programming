0fa708ac2be84c79393b9194ca5d46848fff4de2
Najlae86 <najlae.aissaoui86@gmail.com>
2023-03-16 13:52:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - a program that prints a mine with puts fonction
+ *
+ * return: always 0 (Succes)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
