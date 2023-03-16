9b1ce91a53a0905a481ba1697ded600c08558d4b
Najlae86 <najlae.aissaoui86@gmail.com>
2023-03-16 14:02:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+
 /**
- * main - a program that prints a mine with puts fonction
- *
- * return: always 0 (Succes)
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
