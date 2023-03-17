b508057fc0071ed7da55a6b62c5ad4f3c849588b
Najlae86 <najlae.aissaoui86@gmail.com>
2023-03-17 04:42:56 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,13 +18,13 @@ if (n < 0)
 {
 printf("%d is negative\n", n);
 }
-if else(n > 0)
+if else(n == 0)
 {
-printf("%d is positive\n", n);
+printf("%d is zero\n", n);
 }
 else
 {
-printf("%d is zero\n", n);
+printf("%d is positive\n", n);
 }
 	return (0);
 }
