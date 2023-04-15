1913be8e300d7b859b048f7b44210843ee34b83b
Najlae86 <najlae.aissaoui86@gmail.com>
2023-04-15 07:07:00 -0700

    libmy.a

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,10 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 int _putchar(char c);
 int _islower(int c);
